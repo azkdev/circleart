05a7aad5894301fcd980d6562256497b15b7ae87
azkdev <azkdev@outlook.com>
2018-02-28 23:11:59 +0200

    Menu buttons added.

--- src/main/kotlin/com/azkdev/circleart/view/MainView.kt
+++ src/main/kotlin/com/azkdev/circleart/view/MainView.kt
@@ -1,15 +1,11 @@
 package com.azkdev.circleart.view
 
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView
-import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon
-import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView
 import javafx.application.Platform
+import javafx.beans.binding.Bindings.and
 import javafx.geometry.Insets
-import javafx.geometry.Pos
+import javafx.scene.image.Image
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
-import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class MainView : View() {
@@ -33,30 +29,61 @@ class MainView : View() {
         vbox {
             prefWidth = 50.0
             prefHeight = 500.0
+            spacing = 20.0
+            background = Background(BackgroundFill(c(255, 255, 255, 1.0), CornerRadii.EMPTY, Insets.EMPTY))
+
             setOnMousePressed {
                 offX = primaryStage.x - it.screenX
                 offY = primaryStage.y - it.screenY
             }
+
             setOnMouseDragged {
                 primaryStage.x = it.screenX + offX
                 primaryStage.y = it.screenY + offY
             }
-            background = Background(BackgroundFill(c(255, 255, 255, 1.0), CornerRadii.EMPTY, Insets.EMPTY))
+
+            stackpane {
+                setPrefSize(50.0, 50.0)
+                
+
+                imageview {
+                    image = Image(resources["/icons/power.png"])
+                }
+
+                setOnMouseClicked {
+                    Platform.exit()
+                }
+            }
 
             stackpane {
                 setPrefSize(50.0, 50.0)
 
-                label("", FontAwesomeIconView(FontAwesomeIcon.POWER_OFF).apply {
-                    size = "40px"
-                    style {
-                        fill = c(55, 55, 55, 1.0)
-                    }
-                }) {
-                    setPrefSize(20.0, 20.0)
-                    alignment = Pos.CENTER
-                    setOnMouseClicked {
-                        Platform.exit()
-                    }
+                imageview {
+                    image = Image(resources["/icons/new.png"])
+                }
+            }
+
+            stackpane {
+                setPrefSize(50.0, 50.0)
+
+                imageview {
+                    image = Image(resources["/icons/play.png"])
+                }
+            }
+
+            stackpane {
+                setPrefSize(50.0, 50.0)
+
+                imageview {
+                    image = Image(resources["/icons/pause.png"])
+                }
+            }
+
+            stackpane {
+                setPrefSize(50.0, 50.0)
+
+                imageview {
+                    image = Image(resources["/icons/settings.png"])
                 }
             }
         }
