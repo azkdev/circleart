8583cc31039cfa11d35f0f249d073fcbf38b3272
azkdev <azkdev@outlook.com>
2018-03-01 23:10:09 +0200

    ...

--- src/main/kotlin/com/azkdev/circleart/view/MainView.kt
+++ src/main/kotlin/com/azkdev/circleart/view/MainView.kt
@@ -12,10 +12,9 @@ class MainView : View("Circles") {
             addClass(Styles.canvaswindow)
 
             canvas(500.0, 500.0) {
-                graphicsContext2D.fill = c("#FF0000", 1.0)
-
+                graphicsContext2D.fill = c("#FF0000")
                 (0..10).forEach {
-                    val r: Double = Math.random() * 100
+                    val r: Double = Math.random() * 10
                     graphicsContext2D.fillOval(Math.random() * width, Math.random() * height, r, r)
                 }
             }
