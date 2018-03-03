5f6b55ffc0079d0b81339694174bbce6b864349f
azkdev <azkdev@outlook.com>
2018-03-03 21:48:38 +0200

    Content area view created.

---
+++ src/main/kotlin/com/azkdev/circleart/view/ContentAreaView.kt
@@ -0,0 +1,12 @@
+package com.azkdev.circleart.view
+
+import com.azkdev.circleart.style.Styles
+import tornadofx.*
+
+class ContentAreaView : View() {
+
+    override val root = hbox {
+        addClass(Styles.contentarea)
+    }
+
+}
