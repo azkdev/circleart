dd62f382104981f8eede3a7f9df8c2a590e7007f
azkdev <azkdev@outlook.com>
2018-03-13 00:02:18 +0200

    Timeline implementation.

--- src/main/kotlin/com/azkdev/circleart/view/ContentAreaView.kt
+++ src/main/kotlin/com/azkdev/circleart/view/ContentAreaView.kt
@@ -1,6 +1,8 @@
 package com.azkdev.circleart.view
 
 import com.azkdev.circleart.style.Styles
+import javafx.animation.Timeline
+import javafx.scene.canvas.GraphicsContext
 import tornadofx.*
 
 class ContentAreaView : View() {
@@ -12,12 +14,23 @@ class ContentAreaView : View() {
             canvas(500.0, 500.0) {
                 graphicsContext2D.fill = c("#FFFFFF")
                 graphicsContext2D.fillRect(0.0, 0.0, width, height)
+                timeline {
+                    keyframe(60.millis) {
+                        createCircle(graphicsContext2D)
+                    }
+                }
             }
         }
 
         right {
             this += RecentScketchesView::class
         }
+    }
+
+    private fun createCircle(ctx: GraphicsContext) {
+        ctx.fill = c((Math.random() * 255).toInt(), (Math.random() * 255).toInt(), (Math.random() * 255).toInt())
+        ctx.fillOval(Math.random() * 500, Math.random() * 500, 50.0, 50.0)
+        println("SSD")
     }
 
 }
